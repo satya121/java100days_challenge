6f047dc83174bcb90115e119797ca1c25152d6c1
satya121 <69461444+satya121@users.noreply.github.com>
2020-09-02 21:17:17 +0530

    Add files via upload

---
+++ Full_Even_Odd.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Full_Even_Odd{
+	public int is_Even(int num){
+	int count=0;
+	int temp;
+	while(num!=0){
+		temp=num%10;
+		if(temp%2==0){
+			 num=num/10; 
+			count++;
+			
+		 }
+		else
+		{
+			 //return count;
+			break;
+		}
+		
+		}
+		return count;
+	}
+	public int is_Odd(int num){
+	int count=0;
+	int temp;
+	while(num!=0){
+		temp=num%10;
+		if(temp%2!=0){
+			num=num/10; 
+			count++;
+		 }
+		else
+		{
+			//return count;
+			break;
+		}
+		
+		}
+		return count;
+	}
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		Full_Even_Odd obj=new Full_Even_Odd();
+		System.out.println("Enter a number:");
+		int num= sc.nextInt();
+		int l_Digit =num%10;
+		int digits = (int)Math.log10(num)+1;
+		System.out.println("digits=" +digits +"l_Digit=" +l_Digit);
+		if(l_Digit %2==0){
+			int count=obj.is_Even(num);
+			if(count==digits){ System.out.println("Entered number are Even"); }
+		}
+		else{
+			int count=obj.is_Odd(num);
+			if(count==digits){ System.out.println("Entered digits are Odd"); }
+		}
+	}
+}
