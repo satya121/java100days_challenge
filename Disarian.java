32eccfa6125f60c95fc4d11dba6bee9c0220f6af
satya121 <69461444+satya121@users.noreply.github.com>
2020-09-14 17:57:41 +0530

    Add files via upload

---
+++ Disarian.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.Math;
+public class Disarian{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		int digits = (int)Math.log10(num)+1;
+		int temp=num;
+		int sum=0;
+		int rem;
+		while(temp!=0){
+			rem=temp%10;
+			sum=sum+(int)Math.pow(rem,digits);
+			temp=temp/10;
+			digits--;
+		}
+		if(sum==num)
+			System.out.println("Entered number is Disarian number");
+		else
+			System.out.println("Entered number is not Diasarian number");
+	}
+}
+		
+		
