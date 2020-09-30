93aab1b3c5e383f215cf3b81763c68aedb08f9ef
satya121 <69461444+satya121@users.noreply.github.com>
2020-09-30 21:21:04 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,14 @@
+public class pattern2
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				System.out.print("*"  +" ");
+			}
+			System.out.println();
+		}
+	}
+}
