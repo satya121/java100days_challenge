459fc6bde808816865b702fca779c6d18838b95a
satya121 <69461444+satya121@users.noreply.github.com>
2020-10-24 17:38:56 +0530

    Add files via upload

---
+++ Pattern32.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pattern32
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new  Scanner(System.in);
+		int n=sc.nextInt();
+		int num=(n*2)-1;
+		int a=0;
+		for(int i=1;i<=num;i++)
+		{
+			a=(i<=n)?(i):(a-1);
+			for(int j=a;j<n;j++)
+				System.out.print(" ");
+			for(int j=n-1;j>=n-a;j--)
+				System.out.print((char)(j+65) +" ");
+			System.out.println();
+		}
+	}
+}
