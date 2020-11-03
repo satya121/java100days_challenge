7fec687fcc2802bf0b10f71fc41c93c4e75ebda0
satya121 <69461444+satya121@users.noreply.github.com>
2020-11-03 17:06:00 +0530

    Add files via upload

---
+++ Pattern44.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Pattern44
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x=n; int y=n;
+		int flag=1;
+		for(int i=n-1;i>=-(n-1);i--)
+		{
+			if(i<=0){
+				flag=0;
+			}
+			for(int j=1;j<=(2*n)-1;j++)
+			{
+				if(j==x || j==y)
+					System.out.print((char)(64+n-Math.abs(i)));
+				else
+					System.out.print(" ");
+			}
+			if(flag==1){
+				x=x+1;
+				y=y-1; 
+			 }
+			else
+			{
+				x=x-1;
+				y=y+1;
+			}
+			System.out.println();
+		}				
+	}
+}
